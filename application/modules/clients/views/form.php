d9639ef80c4cbb7154ce3b3c291f2dc9f1452e1f
Frank Wohlers <frank.wohlers@spar.at>
2017-03-20 09:12:07 +0100

    Designänderungen
    Signed-off-by: Frank Wohlers <frank.wohlers@spar.at>

--- application/modules/clients/views/form.php
+++ application/modules/clients/views/form.php
@@ -28,8 +28,8 @@
         >
 
         <fieldset>
-            <legend><?php echo trans('personal_information'); ?></legend>
-            <div class="input-group col-xs-6">
+            <label><?php echo trans('personal_information'); ?>: </label>
+            <div class="input-group">
               <span class="input-group-addon">
                 <?php echo trans('active_client'); ?>:
                 <input id="client_active" name="client_active" type="checkbox" value="1"
@@ -47,59 +47,51 @@
         </fieldset>
 
         <div class="row">
-
             <div class="col-xs-12 col-sm-6">
                 <fieldset>
-                    <legend><?php echo trans('address'); ?></legend>
-
+                    <label><?php echo trans('address'); ?>: </label>
+<!-- Straße und Hausnummer -->
                     <div class="form-group">
-                        <label><?php echo trans('street_address'); ?>: </label>
-
                         <div class="controls">
                             <input type="text" name="client_address_1" id="client_address_1" class="form-control"
+                                   placeholder="<?php echo trans('street_address'); ?>"
                                    value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_address_1')); ?>">
                         </div>
                     </div>
-
+<!-- Adress-Zusatz -->
                     <div class="form-group">
-                        <label><?php echo trans('street_address_2'); ?>: </label>
-
                         <div class="controls">
                             <input type="text" name="client_address_2" id="client_address_2" class="form-control"
+                                   placeholder="<?php echo trans('street_address_2'); ?>"
                                    value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_address_2')); ?>">
                         </div>
                     </div>
-
+<!-- PLZ -->
                     <div class="form-group">
-                        <label><?php echo trans('city'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_city" id="client_city" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_city')); ?>">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label><?php echo trans('state'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_state" id="client_state" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_state')); ?>">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label><?php echo trans('zip_code'); ?>: </label>
-
                         <div class="controls">
                             <input type="text" name="client_zip" id="client_zip" class="form-control"
+                                   placeholder="<?php echo trans('zip_code'); ?>"
                                    value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_zip')); ?>">
                         </div>
                     </div>
-
+<!-- Ort -->
                     <div class="form-group">
-                        <label><?php echo trans('country'); ?>: </label>
-
+                        <div class="controls">
+                            <input type="text" name="client_city" id="client_city" class="form-control"
+                                   placeholder="<?php echo trans('city'); ?>"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_city')); ?>">
+                        </div>
+                    </div>
+<!-- Bundesland -->
+                    <div class="form-group">
+                        <div class="controls">
+                            <input type="text" name="client_state" id="client_state" class="form-control"#
+                                   placeholder="<?php echo trans('state'); ?>"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_state')); ?>">
+                        </div>
+                    </div>
+<!-- Land -->
+                    <div class="form-group">
                         <div class="controls">
                             <select name="client_country" id="client_country" class="form-control">
                                 <option></option>
@@ -113,100 +105,12 @@
                             </select>
                         </div>
                     </div>
-                </fieldset>
-            </div>
-
-            <div class="col-xs-12 col-sm-6">
-                <fieldset>
-
-                    <legend><?php echo trans('contact_information'); ?></legend>
-
-                    <div class="form-group">
-                        <label><?php echo trans('phone_number'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_phone" id="client_phone" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_phone')); ?>">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label><?php echo trans('fax_number'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_fax" id="client_fax" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_fax')); ?>">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label><?php echo trans('mobile_number'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_mobile" id="client_mobile" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_mobile')); ?>">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label><?php echo trans('email_address'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_email" id="client_email" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_email')); ?>">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label><?php echo trans('web_address'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_web" id="client_web" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_web')); ?>">
-                        </div>
-                    </div>
-
-                </fieldset>
-            </div>
-
-            <div class="col-xs-12 col-sm-6">
-                <fieldset>
-
-                    <legend><?php echo trans('tax_information'); ?></legend>
-
-                    <div class="form-group">
-                        <label><?php echo trans('vat_id'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_vat_id" id="client_vat_id" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_vat_id')); ?>">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label><?php echo trans('tax_code'); ?>: </label>
-
-                        <div class="controls">
-                            <input type="text" name="client_tax_code" id="client_tax_code" class="form-control"
-                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_tax_code')); ?>">
-                        </div>
-                    </div>
-
-                </fieldset>
-            </div>
-
-        </div>
-
-        <?php if ($custom_fields) { ?>
-            <div class="row">
-                <div class="col-xs-12">
-                    <fieldset>
-                        <legend><?php echo trans('custom_fields'); ?></legend>
+                    <?php if ($custom_fields) { ?>
+                        <label><?php echo trans('custom_fields'); ?></label>
                         <?php foreach ($custom_fields as $custom_field) { ?>
                             <div class="form-group">
-                                <label><?php echo $custom_field->custom_field_label; ?>: </label>
-
-                                <div class="controls">
+                                <div class="input-group" style="width:100%">
+                                    <span class="input-group-addon" style="width:20%"> <?php echo $custom_field->custom_field_label; ?>: </span>
                                     <input type="text" class="form-control"
                                            name="custom[<?php echo $custom_field->custom_field_column; ?>]"
                                            id="<?php echo $custom_field->custom_field_column; ?>"
@@ -214,9 +118,72 @@
                                 </div>
                             </div>
                         <?php } ?>
-                    </fieldset>
-                </div>
+                    <?php } ?>
+                </fieldset>
             </div>
-        <?php } ?>
+
+            <div class="col-xs-12 col-sm-6">
+                <fieldset>
+                    <label><?php echo trans('contact_information'); ?>: </label>
+<!-- Telefon -->
+                    <div class="form-group" >                       
+                        <div class="input-group" style="width:100%">
+                            <span class="input-group-addon" style="width:20%"> <?php echo trans('phone_number'); ?>: </span>
+                            <input type="text" name="client_phone" id="client_phone" class="form-control"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_phone')); ?>">
+                        </div>
+                    </div>
+<!-- Fax -->
+                    <div class="form-group">
+                        <div class="input-group" style="width:100%">
+                            <span class="input-group-addon" style="width:20%"> <?php echo trans('fax_number'); ?>: </span>
+                            <input type="text" name="client_fax" id="client_fax" class="form-control"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_fax')); ?>">
+                        </div>
+                    </div>
+<!-- Mobil -->
+                    <div class="form-group">
+                        <div class="input-group" style="width:100%">
+                            <span class="input-group-addon" style="width:20%"> <?php echo trans('mobile_number'); ?>: </span>
+                            <input type="text" name="client_mobile" id="client_mobile" class="form-control"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_mobile')); ?>">
+                        </div>
+                    </div>
+<!-- Email -->
+                    <div class="form-group">
+                        <div class="input-group" style="width:100%">
+                            <span class="input-group-addon" style="width:20%"> <?php echo trans('email_address'); ?>: </span>
+                            <input type="text" name="client_email" id="client_email" class="form-control"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_email')); ?>">
+                        </div>
+                    </div>
+<!-- Web -->
+                    <div class="form-group">
+                        <div class="input-group" style="width:100%">
+                            <span class="input-group-addon" style="width:20%"> <?php echo trans('web_address'); ?>: </span>
+                            <input type="text" name="client_web" id="client_web" class="form-control"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_web')); ?>">
+                        </div>
+                    </div>
+<!-- UST-ID -->
+                    <label><?php echo trans('tax_information'); ?>: </label>
+                    <div class="form-group">
+                        <div class="input-group" style="width:100%">
+                            <span class="input-group-addon" style="width:20%"> <?php echo trans('vat_id'); ?>: </span>
+                            <input type="text" name="client_vat_id" id="client_vat_id" class="form-control"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_vat_id')); ?>">
+                        </div>
+                    </div>
+<!-- Steuer-Nr. -->
+                    <div class="form-group">
+                        <div class="input-group" style="width:100%">
+                            <span class="input-group-addon" style="width:20%"> <?php echo trans('tax_code'); ?>: </span>
+                            <input type="text" name="client_tax_code" id="client_tax_code" class="form-control"
+                                   value="<?php echo htmlspecialchars($this->mdl_clients->form_value('client_tax_code')); ?>">
+                        </div>
+                    </div>
+                </fieldset>
+            </div>
+        </div>
     </div>
 </form>
--- application/modules/layout/views/layout.php
+++ application/modules/layout/views/layout.php
@@ -151,7 +151,7 @@
                     </ul>
                 </li>
 
-                <li class="dropdown">
+                <li class="dropdown <?php if ($this->mdl_settings->setting('disable_quotes') == 1) {echo 'hidden';} ?>">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-caret-down"></i> &nbsp;<span
                             class="hidden-sm"><?php echo trans('quotes'); ?></span><i
